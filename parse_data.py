6804c35070d2bffc3c1ec33786576fe08a44fc39
dustinm <dmaio0107@gmail.com>
2024-09-13 22:25:22 -0400

    [WIP] parse_data.py

--- parse_data.py
+++ parse_data.py
@@ -3,9 +3,11 @@ import re
 from typing import Optional
 
 def get_block_seller_amount(message) -> Optional[int]: 
+  print(message)
   block_role_id = "1275913207529345166"
   if block_role_id in message["mention_roles"]:
-    numbers = re.findall(r"\d+\.\d+|\d+", message["content"])
+    numbers = re.findall(r"\d+\.\d+|\d+\.?", message["content"])
+    print(numbers)
     if len(numbers) == 0:
       return None
     numbers = [int(x) for x in numbers]
@@ -15,12 +17,23 @@ def get_block_seller_amount(message) -> Optional[int]:
 
 if __name__=="__main__":
   with open("discord_24_data.json") as f: 
-    data = json.load(f)
+    requests = f.readlines()
+  
+  processed_data = []
+  for request in requests:
+    processed_request = request.replace("'", '"')
+    try: 
+      data = json.loads(processed_request)
+    except Exception as e:
+      print("request: ", processed_request)
+      print(e)
+      continue
+    messages = data["messages"]
 
-  messages = data["messages"]
-  data = []
-  for message in messages:
-    amount = get_block_seller_amount(message)
-    if amount is not None:
-      data.append((amount, message["timestamp"]))
-  print(data)
+    for _message in messages:
+      message = _message[0]
+      # print(message)
+      amount = get_block_seller_amount(message)
+      if amount is not None:
+        processed_data.append((amount, message["timestamp"]))
+  print(processed_data)
